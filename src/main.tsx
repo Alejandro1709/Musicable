25813f0c8105bbbf7585bc0718000eb92698abfc
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-11-30 20:30:05 -0500

    Add: React router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Global } from '@emotion/react';
 import { global, reset } from './styles';
 import App from './App';
@@ -8,6 +9,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Global styles={global} />
     <Global styles={reset} />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
