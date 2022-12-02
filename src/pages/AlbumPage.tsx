51edfd5286bc2a546814a53a5b47c556f9b2c404
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-12-02 10:41:20 -0500

    Add: get album by slug in album page

--- src/pages/AlbumPage.tsx
+++ src/pages/AlbumPage.tsx
@@ -1,18 +1,24 @@
+import { useContext } from 'react';
+import { AlbumContext } from '../context/albumContext';
 import { useParams } from 'react-router-dom';
 import Navigator from '../components/Navigator';
 
 function AlbumPage() {
   const { albumSlug } = useParams();
 
+  const { initialAlbums } = useContext(AlbumContext);
+
+  const album = initialAlbums.find((album) => album.albumSlug === albumSlug);
+
   return (
     <>
       <Navigator
         backUrl='/'
-        title='Title'
+        title={album?.albumTitle || 'Album'}
         rightTitle='Edit Album'
         rightUrl={`/albums/${albumSlug}/edit`}
       />
-      <h1>{albumSlug} Page</h1>
+      <h1>Hello There!</h1>
     </>
   );
 }
